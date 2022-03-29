a06b156708a3dfa222b0f8bfe149d9c18c868cf4
Dmitry Korolev <dkorolev13@gmail.com>
2022-03-29 10:14:47 +0300

    Получил file_id для картинки

--- client/main.py
+++ client/main.py
@@ -2,6 +2,8 @@ from aiogram import Bot, types, Dispatcher
 from aiogram.types import ContentType
 from aiogram.utils import executor
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 bot = Bot(token='5057381153:AAHPgJ3HpqDKkjrBq4CH1Me0dgClypbqYQk')
 
@@ -16,16 +18,27 @@ async def get_message(message: types.Message):
     print(message.from_user)
     print(message.photo[0].file_id)
 
-    features = {
-        "file_id": message.photo[0].file_id
-    }
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
 
-    resp = requests.post(
-        "http:// 172.18.0.1:35800/predict",
-        json=features
-    )
+    session.get('http://127.0.0.1:80/pred')
 
-    text = 'это фото'
-    await message.answer(text=text)
+    #resp = requests.get('http://localhost:80')
+
+    # features = {
+    #     "file_id": message.photo[0].file_id
+    # }
+    #
+    # resp = requests.post(
+    #     "http:// 172.18.0.1:35800/predict",
+    #     json=features
+    # )
+
+    ans = resp.json()
+    #text = 'это фото'
+    #await message.answer(text=ans)
 
 executor.start_polling(dp)
