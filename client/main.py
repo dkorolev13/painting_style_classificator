346148248dd38be3ee4c7a49d4a842ee7bcdfbb2
Dmitry Korolev <dkorolev13@gmail.com>
2022-04-08 17:57:09 +0300

    Add .env to /server

--- client/main.py
+++ client/main.py
@@ -4,8 +4,6 @@ from aiogram.utils import executor
 import requests
 from config import BOT_TOKEN
 
-
-
 bot = Bot(token=BOT_TOKEN)
 
 dp = Dispatcher(bot)
@@ -13,12 +11,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(content_types=ContentType.PHOTO)  # фото
 async def get_message(message: types.Message):
-    # print(message)
-    # print(message.from_user)
-    # print(message.photo[0].file_id)
-
     resp = requests.get("http://127.0.0.1/" + message.photo[-1].file_id)
-
     await message.answer(text=resp.json())
 
 
--- server/server.py
+++ server/server.py
@@ -3,29 +3,15 @@ from fastai.train import load_learner
 from fastai.vision import open_image
 from torch import topk
 from aiogram import Bot
-import shutil
+
+#from client.config import BOT_TOKEN
+#from client.main import bot
+from config import BOT_TOKEN
 
 app = FastAPI()
 learner = load_learner('./', 'model.pkl')
-bot = Bot(token='5057381153:AAHPgJ3HpqDKkjrBq4CH1Me0dgClypbqYQk')
-
-
-def print_pred_probs(pred, k):
-    topk_pred = topk(pred[2], k)
-    #return f'{learner.data.classes[topk_pred.indices[0]]}: {100 * topk_pred.values[0]:.2f}%' - работает
-    res = [f'{learner.data.classes[topk_pred.indices[i]]}: {100 * topk_pred.values[i]:.2f}%' for i in range(3)]
-
-    # res = {}
-    # for i in range(3):
-    #     res = {f'{learner.data.classes[topk_pred.indices[i]]}': f'{100 * topk_pred.values[i]:.2f}%'}
-    #     #print(f'{learner.data.classes[topk_pred.indices[i]]}: {100 * topk_pred.values[i]:.2f}%')
-    return res
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello, my 13 best friend"}
-
+bot = Bot(token=BOT_TOKEN)
+#bot = Bot(token="5057381153:AAHPgJ3HpqDKkjrBq4CH1Me0dgClypbqYQk")
 
 @app.get("/{file_id}")
 async def get_file_id(file_id: str):
@@ -35,18 +21,14 @@ async def get_file_id(file_id: str):
 
     painting = open_image('image_for_prediction.jpg')
     pred = learner.predict(painting)
-    #res = print_pred_probs(pred, 3)
+    # res = print_pred_probs(pred, 3)
     res = print_pred_probs(pred, 3)
     res_str = '\n'.join(res)
     return res_str
 
-# @app.post("/imgs/")
-# async def root2(img: UploadFile):
-#     #with open('test.jpeg', "wb") as buffer:
-#         #shutil.copyfileobj(img.file, buffer)
-#
-#     painting = open_image(img.file)
-#     pred = learner.predict(painting)
-#     res=print_pred_probs(pred, 3)
-#
-#     await {"file_name": res}
+
+def print_pred_probs(pred, k):
+    topk_pred = topk(pred[2], k)
+    # return f'{learner.data.classes[topk_pred.indices[0]]}: {100 * topk_pred.values[0]:.2f}%' - работает
+    res = [f'{learner.data.classes[topk_pred.indices[i]]}: {100 * topk_pred.values[i]:.2f}%' for i in range(3)]
+    return res
