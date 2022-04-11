4d427f98f330c7536984175f3ebc403629d08dab
Dmitry Korolev <dkorolev13@gmail.com>
2022-04-11 19:26:26 +0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,17 +1,22 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI
+
 from fastai.train import load_learner
 from fastai.vision import open_image
 from torch import topk
+
 from aiogram import Bot
 
-#from client.config import BOT_TOKEN
-#from client.main import bot
 from config import BOT_TOKEN
+
+
+def pred_probs(predictions, k):
+    topk_pred = topk(predictions[2], k)
+    res = [f'{learner.data.classes[topk_pred.indices[i]]}: {100 * topk_pred.values[i]:.2f}%' for i in range(k)]
+    return res
 
 app = FastAPI()
 learner = load_learner('./', 'model.pkl')
 bot = Bot(token=BOT_TOKEN)
-#bot = Bot(token="5057381153:AAHPgJ3HpqDKkjrBq4CH1Me0dgClypbqYQk")
 
 @app.get("/{file_id}")
 async def get_file_id(file_id: str):
@@ -20,15 +25,7 @@ async def get_file_id(file_id: str):
     await bot.download_file(file_path, 'image_for_prediction.jpg')
 
     painting = open_image('image_for_prediction.jpg')
-    pred = learner.predict(painting)
-    # res = print_pred_probs(pred, 3)
-    res = print_pred_probs(pred, 3)
+    predictions = learner.predict(painting)
+    res = pred_probs(predictions, 3)
     res_str = '\n'.join(res)
     return res_str
-
-
-def print_pred_probs(pred, k):
-    topk_pred = topk(pred[2], k)
-    # return f'{learner.data.classes[topk_pred.indices[0]]}: {100 * topk_pred.values[0]:.2f}%' - работает
-    res = [f'{learner.data.classes[topk_pred.indices[i]]}: {100 * topk_pred.values[i]:.2f}%' for i in range(3)]
-    return res
