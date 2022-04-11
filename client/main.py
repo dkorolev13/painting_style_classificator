88b03bfa8a2f52b171c183871395482a142fdb56
Dmitry Korolev <dkorolev13@gmail.com>
2022-04-11 19:23:38 +0300

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, types, Dispatcher
 from aiogram.types import ContentType
 from aiogram.utils import executor
+
 import requests
+
 from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
@@ -9,9 +11,12 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(content_types=ContentType.PHOTO)  # фото
+@dp.message_handler(content_types=ContentType.PHOTO)
 async def get_message(message: types.Message):
-    resp = requests.get("http://127.0.0.1/" + message.photo[-1].file_id)
+    public_ip = "http://130.162.33.39:5000/"
+    # public_ip = "http://127.0.0.1/"
+    file_id = message.photo[-1].file_id
+    resp = requests.get(public_ip + file_id)
     await message.answer(text=resp.json())
 
 
